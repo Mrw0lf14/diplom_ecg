f7f42e81a37473d4bce9d645ae79694a9a9a7e83
Alexei <mstrwlf7@gmail.com>
2025-02-28 20:36:13 +0300

    [+] added microphone

--- holter/holter.ino
+++ holter/holter.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <driver/i2s.h>
 
 #define MMA8452_ADDRESS 0x1C  // Адрес датчика
 #define REG_WHO_AM_I 0x0D     // Регистр идентификатора
@@ -8,6 +9,10 @@
 #define SDA_PIN 21  // SDA для ESP32 WROOM
 #define SCL_PIN 22  // SCL для ESP32 WROOM
 
+#define I2S_WS 25   // Word Select (LRCLK)
+#define I2S_SD 26   // Serial Data (DOUT)
+#define I2S_SCK 27  // Bit Clock (BCLK)
+
 void setup() {
     Serial.begin(115200);
     Wire.begin(SDA_PIN, SCL_PIN);
@@ -16,6 +21,8 @@ void setup() {
         Serial.println("Ошибка инициализации MMA8452");
         while (1);
     }
+    
+    initializeI2S();
 }
 
 void loop() {
@@ -26,7 +33,9 @@ void loop() {
     Serial.print(" Y: "); Serial.print(y);
     Serial.print(" Z: "); Serial.println(z);
     
-    delay(500);
+    readMicrophone();
+    
+    delay(100);
 }
 
 bool initializeMMA8452() {
@@ -63,4 +72,38 @@ void readAccelerometer(int16_t &x, int16_t &y, int16_t &z) {
         
         x >>= 4; y >>= 4; z >>= 4;  // Приведение к 12-битному формату
     }
+}
+
+void initializeI2S() {
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+        .sample_rate = 16000,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+        .intr_alloc_flags = 0,
+        .dma_buf_count = 8,
+        .dma_buf_len = 64,
+        .use_apll = false,
+        .tx_desc_auto_clear = false,
+        .fixed_mclk = 0
+    };
+    
+    i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_SCK,
+        .ws_io_num = I2S_WS,
+        .data_out_num = I2S_PIN_NO_CHANGE,
+        .data_in_num = I2S_SD
+    };
+    
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+}
+
+void readMicrophone() {
+    int16_t sample_buffer[64];
+    size_t bytes_read;
+    i2s_read(I2S_NUM_0, sample_buffer, sizeof(sample_buffer), &bytes_read, portMAX_DELAY);
+    
+    Serial.print("Mic Sample: "); Serial.println(sample_buffer[0]);
 }
